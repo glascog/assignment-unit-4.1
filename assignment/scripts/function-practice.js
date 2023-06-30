df2c41403e8cf9d9c7e1c50b3bef4330b2d2b37f
Gabe Glasco <gabeglasco@gmail.com>
2023-06-30 12:07:15 -0500

    fixed number 7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -9,7 +9,7 @@ function hello() {
   return 'Hello World!';
 }
 // Call the function to test
-console.log('Test - should say "Hello World!"', hello());
+// console.log('Test - should say "Hello World!"', hello());
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
@@ -18,20 +18,20 @@ function helloName(name) {
   return 'Hello, Your Name!';
 }
 // Remember to call the function to test
-console.log('Test - should say "Hello, Your Name!"', helloName());
+// console.log('Test - should say "Hello, Your Name!"', helloName());
 
 // 3. Function to add two numbers together & return the result
 function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
   return firstNumber + secondNumber;
 }
-console.log('Test - should return the sum of two numbers:', addNumbers(3,4));
+// console.log('Test - should return the sum of two numbers:', addNumbers(3,4));
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree(num1, num2, num3) {
   return num1 * num2 * num3;
 }
-console.log('Test - should return the product of three numbers:', multiplyThree(5,6,7));
+// console.log('Test - should return the product of three numbers:', multiplyThree(5,6,7));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
@@ -45,9 +45,9 @@ function isPositive(number) {
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log('isPositive - should say true', isPositive(3));
-console.log('isPositive - should say false', isPositive(0));
-console.log('isPositive - should say false', isPositive(-3));
+// console.log('isPositive - should say true', isPositive(3));
+// console.log('isPositive - should say false', isPositive(0));
+// console.log('isPositive - should say false', isPositive(-3));
 
 
 // 6. Function to return the _last_ item in an array. If the 
@@ -55,24 +55,29 @@ console.log('isPositive - should say false', isPositive(-3));
 function getLast(array) {
   return array[array.length - 1]
 }
-console.log('getLast - should return last item in array:', getLast([1, 2, 3]));
-console.log('getLast - should return last item in array:', getLast([]));
+// console.log('getLast - should return last item in array:', getLast([1, 2, 3]));
+// console.log('getLast - should return last item in array:', getLast([]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
+
 function find(value, array) {
-  for(value in array){
-    if(value === array){
-      return true; 
-    } else {
-      return false;
-    }
-}}
+  let result = false;
+  console.log('find value within array:', value, array);
+  for(let item of array){
+    console.log('item is:', item, 'array is:', array);
+    if(item === value){
+      console.log('found value in array')
+      result = true; 
+    } 
+  }
+  return result;
+}
 console.log('find - should say true:', find(3, [2, 3, 4]));
 console.log('find - should say false:', find(5, [2, 3, 4]));
-console.log('find - should say false:', find());
-
+console.log('find - should say false:', find( ));
+console.log('-----------------------------------------------')
 
 // ----------------------
 // Stretch Goals
@@ -82,8 +87,8 @@ console.log('find - should say false:', find());
 function isFirstLetter(letter, string) {
 
 }
-console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
-console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
+// console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
+// console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll() {
