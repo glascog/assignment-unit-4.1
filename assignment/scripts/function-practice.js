15992420082acd8b239d610915d56f5badfb0de4
Gabe Glasco <gabeglasco@gmail.com>
2023-06-30 22:32:33 -0500

    updated number 2

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -9,29 +9,29 @@ function hello() {
   return 'Hello World!';
 }
 // Call the function to test
-// console.log('Test - should say "Hello World!"', hello());
+console.log('Test - should say "Hello World!"', hello());
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return 'Hello, Your Name!';
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-// console.log('Test - should say "Hello, Your Name!"', helloName());
+console.log('Test - should say "Hello, Your Name!"', helloName('Gabe'));
 
 // 3. Function to add two numbers together & return the result
 function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
   return firstNumber + secondNumber;
 }
-// console.log('Test - should return the sum of two numbers:', addNumbers(3,4));
+console.log('Test - should return the sum of two numbers:', addNumbers(3,4));
 
 // 4. Function to multiply three numbers & return the result
 function multiplyThree(num1, num2, num3) {
   return num1 * num2 * num3;
 }
-// console.log('Test - should return the product of three numbers:', multiplyThree(5,6,7));
+console.log('Test - should return the product of three numbers:', multiplyThree(5,6,7));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
@@ -41,13 +41,13 @@ function isPositive(number) {
   }
   else if (number <=0){
     return false;
-}
+  }
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-// console.log('isPositive - should say true', isPositive(3));
-// console.log('isPositive - should say false', isPositive(0));
-// console.log('isPositive - should say false', isPositive(-3));
+console.log('isPositive - should say true', isPositive(3));
+console.log('isPositive - should say false', isPositive(0));
+console.log('isPositive - should say false', isPositive(-3));
 
 
 // 6. Function to return the _last_ item in an array. If the 
@@ -55,8 +55,8 @@ function isPositive(number) {
 function getLast(array) {
   return array[array.length - 1]
 }
-// console.log('getLast - should return last item in array:', getLast([1, 2, 3]));
-// console.log('getLast - should return last item in array:', getLast([]));
+console.log('getLast - should return last item in array:', getLast([1, 2, 3]));
+console.log('getLast - should return last item in array:', getLast([]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
@@ -76,8 +76,6 @@ function find(value, array) {
 }
 console.log('find - should say true:', find(3, [2, 3, 4]));
 console.log('find - should say false:', find(5, [2, 3, 4]));
-console.log('find - should say false:', find( ));
-console.log('-----------------------------------------------')
 
 // ----------------------
 // Stretch Goals
